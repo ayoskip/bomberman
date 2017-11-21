12f34568cffd22cbbc836392a4b5df81a6dfeb07
ayoskip <stephonmskipper@gmail.com>
2017-11-20 22:41:15 -0500

    Add existing file

---
+++ template/js/main.js
@@ -0,0 +1,20 @@
+var pane = $('#pane'),
+    box = $('#bomberman'),
+    w = pane.width() - box.width(),
+    d = {},
+    x = 3;
+
+function newv(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > w ? w : n;
+}
+
+$(window).keydown(function(e) { d[e.which] = true; });
+$(window).keyup(function(e) { d[e.which] = false; });
+
+setInterval(function() {
+    box.css({
+        left: function(i,v) { return newv(v, 37, 39); },
+        top: function(i,v) { return newv(v, 38, 40); }
+    });
+}, 20);
